9374616a587f1547ca216103cd31aaf280835f62
Thomas Dehaeze <dehaeze.thomas@gmail.com>
2017-03-06 19:03:29 +0100

    add shadow with noise

--- main.cpp
+++ main.cpp
@@ -3,14 +3,6 @@
 #define EPSILON 0.001
 #define Z_CAMERA 60
 
-/* /1* get the normalize vector normal to the sphere on the intersection point *1/ */
-/* Vector getIntersectNormal(double dist_sphere, Ray ray, Sphere sphere) */
-/* { */
-/*     Vector intersect_normal = ray.origin + dist_sphere*ray.direction - sphere.origin; */
-/*     intersect_normal.Normalize(); */
-/*     return intersect_normal; */
-/* } */
-
 int main()
 {
     const int W = 1024; /* width: number of pixels */
@@ -23,15 +15,17 @@ int main()
 
     Light light = Light(Vector(-20, -20, 50), 100000000); /* setup the light source position and luminosity */
 
-    Material material = Material(Vector(1, 0, 0));
+    Material material = Material();
 
     Sphere* sphere = new Sphere(Vector(0, 0, -70+Z_CAMERA), 10, &material);
     Sphere* sphere_bis = new Sphere(Vector(0, 20, -70+Z_CAMERA), 5, &material);
+    Sphere* sphere_bottom = new Sphere(Vector(0, 10040, 0), 10000, &material);
 
     Scene scene = Scene();
 
     scene.addSphere(sphere);
     scene.addSphere(sphere_bis);
+    scene.addSphere(sphere_bottom);
 
     std::vector<unsigned char> pixels(3*H*W,0); /* define the array of pixels */
 
@@ -41,18 +35,28 @@ int main()
             /* ray = Ray(i, j, W, H, fov, center); /1* get the initial ray object for one pixel *1/ */
             ray = Ray(center, Vector(j-W/2+0.5, i-H/2+0.5, -H/(2*std::tan(2*M_PI*fov/2/360))));
             Object* intersect_object = scene.getIntersectedObject(ray);
-            if (intersect_object == 0) {
-                pixels[H*i+j]       = 0;
-                pixels[H*i+j+H*W]   = 0;
-                pixels[H*i+j+2*H*W] = 0;
-            } else {
-                double intensity = intersect_object->getIntensity(ray, light);
-                double red   = std::min(255.0, std::pow(intensity*intersect_object->getMaterial()->getColor()[0], 1./2.2));
-                double green = std::min(255.0, std::pow(intensity*intersect_object->getMaterial()->getColor()[1], 1./2.2));
-                double blue  = std::min(255.0, std::pow(intensity*intersect_object->getMaterial()->getColor()[2], 1./2.2));
-                pixels[H*i+j]       = red;
-                pixels[H*i+j+H*W]   = green;
-                pixels[H*i+j+2*H*W] = blue;
+            if (intersect_object != 0) {
+                if (intersect_object->getMaterial()->getReflectivity() != 0.) {
+
+                }
+
+                if (intersect_object->getMaterial()->getTransparency() != 0.) {
+
+                }
+
+                Ray ray_to_light = Ray(*scene.getIntersect(ray), light);
+                Object* intersect_object_light = scene.getIntersectedObject(ray_to_light);
+
+                if (intersect_object_light == 0 || (intersect_object_light != 0 && intersect_object_light->getDistance(ray_to_light) > (*intersect_object->getIntersect(ray) - light.getOrigin()).norm())) {
+                    double intensity = intersect_object->getIntensity(ray, light);
+                    double red   = std::min(255.0, std::pow(intensity*intersect_object->getMaterial()->getColor()[0], 1./2.2));
+                    double green = std::min(255.0, std::pow(intensity*intersect_object->getMaterial()->getColor()[1], 1./2.2));
+                    double blue  = std::min(255.0, std::pow(intensity*intersect_object->getMaterial()->getColor()[2], 1./2.2));
+                    pixels[H*i+j]       = red;
+                    pixels[H*i+j+H*W]   = green;
+                    pixels[H*i+j+2*H*W] = blue;
+                }
+
             }
         }
         if (i%10 == 0)
